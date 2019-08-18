57c3a86f7de7e4500dff84d5855d2e50d29a1f5c
venkatzgithub <imvenkatprasad@gmail.com>
2019-08-18 12:39:38 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Header from "./components/header/header.component";
 import {Homepage} from './pages/homepage/homepage.component';
 import Shop from './pages/shoppage/shop.component';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route,Switch} from 'react-router-dom';
 //import {Hats} from './components/hats/hats.component';
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Switch>
     <Route exact={true} path='/' component={Homepage} />
     <Route  path='/shop' component={Shop} />
+    </Switch>
     </div>
   );
 }
